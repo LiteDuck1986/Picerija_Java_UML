8b7cdc8c105994d1ace379ab3b984da9824e6e8e
LiteDuck1986 <emils2007smirnovs@gmail.com>
2026-01-14 00:04:43 +0200

    Programmas pogas, paneļu, labeļu, Check Box un text Field izveide sākuma posms, kā arī Pasūtitājs klases izveide, vēlāk tiks veikti pielabojumi.

--- src/Pasutitajs.java
+++ src/Pasutitajs.java
@@ -1,4 +1,107 @@
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pasutitajs {
 
+	// 		================ ATRIBŪTI ================
+	
+			private String PasutitajaVards, adrese, talrunis, piedevas, merces;
+			private boolean UzVietas;
+			private double cena;
+			private int PLielums, PasutijumaID;
+			
+			
+			// ========== KONSTRUKTORS ==========
+			
+			public Pasutitajs(int PLielums, int PasutijumaID, double cena, boolean UzVietas, String PasutitajaVards, String adrese, String talrunis,
+					String piedevas, String merces) {
+				this.PLielums = PLielums;
+				this.PasutijumaID = PasutijumaID;
+				this.cena = cena;
+				this.UzVietas = UzVietas;
+				this.PasutitajaVards = PasutitajaVards;
+				this.adrese = adrese;
+				this.talrunis = talrunis;
+				this.piedevas = piedevas;
+				this.merces = merces;
+			}
+			
+			//=============== GETTER METODES ==================
+			
+			public int getPLielums() {
+				return PLielums;
+			}
+			
+			public int getPasutijumaID() {
+				return PasutijumaID;
+			}
+			
+			public double getCena() {
+				return cena;
+			}
+			
+			public boolean getUzVietas() {
+				return UzVietas;
+			}
+			
+			public String getVards() {
+				return PasutitajaVards;
+			}
+			
+			public String getAdrese() {
+				return adrese;
+			}
+			
+			public String getTalrunis() {
+				return talrunis;
+			}
+			
+			public String getPiedevas() {
+				return piedevas;
+			}
+			
+			public String getMerces() {
+				return merces;
+			}
+			
+			
+			
+			//=============== SETTER METODES ==================
+			
+			public void setPLielums(int PLielums) {
+				this.PLielums = PLielums;
+			}
+			
+			public void setPasutijumaID(int PasutijumaID) {
+				this.PasutijumaID = PasutijumaID;
+			}
+			
+			public void setCena(double cena) {
+				this.cena = cena;
+			}
+			
+			public void setUzVietas(boolean UzVietas) {
+				this.UzVietas = UzVietas;
+			}
+			
+			public void setPasutitajaVards(String PasutitajaVards) {
+				this.PasutitajaVards = PasutitajaVards;
+			}
+			
+			public void setAdrese(String adrese) {
+				this.adrese = adrese;
+			}
+			
+			public void setTalrunis(String talrunis) {
+				this.talrunis = talrunis;
+			}
+			
+			public void setPiedevas(String piedevas) {
+				this.piedevas = piedevas;
+			}
+			
+			public void setMerces(String merces) {
+				this.merces = merces;
+			}
 }
--- src/Picerija.java
+++ src/Picerija.java
@@ -3,17 +3,22 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Picerija extends JFrame implements ActionListener{
+
+public class Picerija {
 	
 	static String ievade;
+	
+	public static Pasutitajs pasutitajs = null;
 	
 	// Frame
 	static JFrame frame;
@@ -21,21 +26,28 @@ public class Picerija extends JFrame implements ActionListener{
 	static JPanel galvenaisPanel;
 	
 	// JTextField
-    static JTextField t;
+    static JTextField t, PVards, talrunis, adrese;
     
- // label to display text
-    static JLabel l;
+    // JCheckBox
+    static JCheckBox S, M, XL, senes, ananas, pepperoni, merce, UzVietas;
     
- // default constructor
-    Picerija()
-    {
-    }
+    // labels
+    static JLabel izmersLabel, piedevasLabel;
+    
+    
+    
+    // picas static mainīgie ko izmantoja priekš pasūtijuma izveides
+    static int PicasIzmers = 10;
+    static String PicasPiedevas = "";
+    
+    // Krāsas izveide priekš programmas fona
+    public final static Color sarkansTums = new Color(160, 5, 0);
 	
 	
 
 	public static void main(String[] args) {
 
-		frame = new JFrame("Picerija");
+		frame = new JFrame("Picerijas pasūtijuma programma");
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
@@ -48,42 +60,113 @@ public class Picerija extends JFrame implements ActionListener{
 
         // ============= Paneļi ==============
         JPanel sakumsPanel = new JPanel(null);
-        sakumsPanel.setBackground(Color.white);
+        sakumsPanel.setBackground(sarkansTums);
 
-        JPanel picasIzveidePanel = new JPanel(null);
-        picasIzveidePanel.setBackground(Color.black);
+        JPanel PasutijumaIzveidePanel = new JPanel(null);
+        PasutijumaIzveidePanel.setBackground(sarkansTums);
+        
+        JPanel RedigetPanel = new JPanel(null);
+        RedigetPanel.setBackground(sarkansTums);
         
 
         // ============= PANEĻU SAVIENOJUMS ==============
-        galvenaisPanel.add(sakumsPanel, "sakums");
-        galvenaisPanel.add(picasIzveidePanel, "pica");
+        galvenaisPanel.add(sakumsPanel, "sākums");
+        galvenaisPanel.add(PasutijumaIzveidePanel, "pica");
+        galvenaisPanel.add(RedigetPanel, "rediģēt");
         
-     // create a label to display text
-        l = new JLabel("nothing entered");
-        l.setBounds(300, 260, 160, 20);
+     
         
         // Pogas izveide (SADAĻAS)
-        JButton PicasPoga = new JButton("Pica");
-        PicasPoga.setBounds(10, 225, 160, 50);
-        PicasPoga.setBackground(Color.BLACK);
-        PicasPoga.setForeground(Color.WHITE);
-        PicasPoga.setBorderPainted(true);
-        PicasPoga.setFocusPainted(false);
-        sakumsPanel.add(PicasPoga);
+        JButton SakumaPoga = new JButton("Sākums");
+        SakumaPoga.setBounds(10, 75, 160, 50);
+        SakumaPoga.setBackground(Color.RED);
+        SakumaPoga.setForeground(Color.WHITE);
+        SakumaPoga.setBorderPainted(true);
+        SakumaPoga.setFocusPainted(false);
+        sakumsPanel.add(SakumaPoga);
         
-        JButton B = new JButton("Update");
-        B.setBounds(300, 400, 160, 20);
+        JButton PasutijumaPoga = new JButton("Pievienot pasūtijumu");
+        PasutijumaPoga.setBounds(10, 150, 160, 50);
+        PasutijumaPoga.setBackground(Color.RED);
+        PasutijumaPoga.setForeground(Color.WHITE);
+        PasutijumaPoga.setBorderPainted(true);
+        PasutijumaPoga.setFocusPainted(false);
+        sakumsPanel.add(PasutijumaPoga);
         
-        Picerija text = new Picerija();
         
-        B.addActionListener(text);
+        JButton RedigetPasutijumuPoga = new JButton("Rediģēt pasūtijumus");
+        RedigetPasutijumuPoga.setBounds(10, 225, 160, 50);
+        RedigetPasutijumuPoga.setBackground(Color.RED);
+        RedigetPasutijumuPoga.setForeground(Color.WHITE);
+        RedigetPasutijumuPoga.setBorderPainted(true);
+        RedigetPasutijumuPoga.setFocusPainted(false);
+        sakumsPanel.add(RedigetPasutijumuPoga);
         
-        t = new JTextField("enter the text", 16);
-        t.setBounds(300, 225, 160, 20);
         
-        sakumsPanel.add(t);
-        sakumsPanel.add(B);
-        sakumsPanel.add(l);
+        JButton PievienotPasutijumu = new JButton("Pievienot");
+        PievienotPasutijumu.setBounds(300, 400, 160, 20);
+        
+        // JTextFieldsss
+        t = new JTextField("67", 16);
+        t.setBounds(300, 350, 160, 20);
+        
+        PVards = new JTextField("Jānis", 16);
+        PVards.setBounds(300, 325, 160, 20);
+        
+        // JCheckBox (Picas izmērs)
+        S = new JCheckBox("10");
+        S.setBounds(250, 275, 40, 20);
+        S.setForeground(Color.WHITE);
+        S.setOpaque(false);
+        
+        M = new JCheckBox("15");
+        M.setBounds(300, 275, 40, 20);
+        M.setForeground(Color.WHITE);
+        M.setOpaque(false);
+        
+        XL = new JCheckBox("20");
+        XL.setBounds(350, 275, 40, 20);
+        XL.setForeground(Color.WHITE);
+        XL.setOpaque(false);
+        
+        // Labels
+        izmersLabel = new JLabel("Picas izmērs:");
+        izmersLabel.setBounds(250, 250, 160, 20);
+        izmersLabel.setForeground(Color.WHITE);
+        
+        
+        // Picas piedevas
+        senes = new JCheckBox("Sēnes");
+        senes.setBounds(250, 150, 65, 20);
+        senes.setForeground(Color.WHITE);
+        senes.setOpaque(false);
+        
+        pepperoni = new JCheckBox("Pepperoni");
+        pepperoni.setBounds(315, 150, 90, 20);
+        pepperoni.setForeground(Color.WHITE);
+        pepperoni.setOpaque(false);
+        
+        ananas = new JCheckBox("Ananāss");
+        ananas.setBounds(410, 150, 80, 20);
+        ananas.setForeground(Color.WHITE);
+        ananas.setOpaque(false);
+        
+        // labels
+        piedevasLabel = new JLabel("Picas piedevas:");
+        piedevasLabel.setBounds(250, 125, 160, 20);
+        piedevasLabel.setForeground(Color.WHITE);
+        
+        
+        PasutijumaIzveidePanel.add(t);
+        PasutijumaIzveidePanel.add(PievienotPasutijumu);
+        PasutijumaIzveidePanel.add(izmersLabel);
+        PasutijumaIzveidePanel.add(piedevasLabel);
+        PasutijumaIzveidePanel.add(S);
+        PasutijumaIzveidePanel.add(M);
+        PasutijumaIzveidePanel.add(XL);
+        PasutijumaIzveidePanel.add(senes);
+        PasutijumaIzveidePanel.add(pepperoni);
+        PasutijumaIzveidePanel.add(ananas);
         
         frame.add(galvenaisPanel);
         frame.setVisible(true);
@@ -91,27 +174,115 @@ public class Picerija extends JFrame implements ActionListener{
         
         
         
-//        PicasPoga.addActionListener(e -> {
-//        	nokertBilde.setVisible(false);
-//        	NokertPoga.setVisible(true);
-//        	nokerts.setVisible(false);
-//        	OkPoga.setVisible(false);
-//        	mysteryPokemons.setVisible(true);
-//        	
-//        });
+        ArrayList<Pasutitajs> Pasutitaji = new ArrayList<>();
         
-	}
-        public void actionPerformed(ActionEvent e)
-        {
-            String s = e.getActionCommand();
-            if (s.equals("Update")) {
-                // set the text of the label to the text of the field
-                l.setText(t.getText());
+        
+        
+        
+        // ActionListeners  =================================================================================================
+        PievienotPasutijumu.addActionListener(e -> {
 
-                // set the text of field to blank
-                t.setText(t.getText());
+        	int izmers = PicasIzmers;
+        	
+        	String piedeva = PicasPiedevas;
+        	
+        	Pasutitajs pasutijums = new Pasutitajs(izmers, 1, 1.00, true, "yes", "yes", "yes", piedeva, "yes");
+        	Pasutitaji.add(pasutijums);
+        	
+            izmersLabel.setText(t.getText());
+
+            t.setText(t.getText());
+        	
+        });
+        
+        SakumaPoga.addActionListener(e -> {
+        	sakumsPanel.add(PasutijumaPoga);
+        	sakumsPanel.add(RedigetPasutijumuPoga);
+        	sakumsPanel.add(SakumaPoga);
+        	cardLayout.show(galvenaisPanel, "sākums");
+        });
+        
+        PasutijumaPoga.addActionListener(e -> {
+        	PasutijumaIzveidePanel.add(PasutijumaPoga);
+        	PasutijumaIzveidePanel.add(RedigetPasutijumuPoga);
+        	PasutijumaIzveidePanel.add(SakumaPoga);
+        	cardLayout.show(galvenaisPanel, "pica");
+        });
+        
+        RedigetPasutijumuPoga.addActionListener(e -> {
+        	RedigetPanel.add(RedigetPasutijumuPoga);
+        	RedigetPanel.add(PasutijumaPoga);
+        	RedigetPanel.add(SakumaPoga);
+        	cardLayout.show(galvenaisPanel, "rediģēt");
+        });
+        
+        
+        // Action Listener priekš JCheckBox
+        ActionListener klausitajs = e -> {
+            JCheckBox izveletais = (JCheckBox) e.getSource();
+
+            // Picas izmērs sadaļa ==========================================================
+            if (izveletais == S && S.isSelected()) {
+                M.setSelected(false);
+                XL.setSelected(false);
+                PicasIzmers = 10;
+            }
+            if (izveletais == M && M.isSelected()) {
+                S.setSelected(false);
+                XL.setSelected(false);
+                PicasIzmers = 15;
+            }
+            if (izveletais == XL && XL.isSelected()) {
+                S.setSelected(false);
+                M.setSelected(false);
+                PicasIzmers = 20;
             }
 
+            // Ja lietotājs visu ir uncheckojis tad Picas izmērs būs 10
+            if (!S.isSelected() && !M.isSelected() && !XL.isSelected()) {
+            	PicasIzmers = 10;
+            }
+            
+            
+            
+            // Picas piedevumi sadaļa ====================================================
+            if (izveletais == senes && senes.isSelected()) {
+                pepperoni.setSelected(false);
+                ananas.setSelected(false);
+                PicasPiedevas = "Sēnes";
+            }
+            if (izveletais == pepperoni && pepperoni.isSelected()) {
+            	senes.setSelected(false);
+                ananas.setSelected(false);
+                PicasPiedevas = "Pepperoni";
+            }
+            if (izveletais == ananas && ananas.isSelected()) {
+            	senes.setSelected(false);
+            	pepperoni.setSelected(false);
+                PicasPiedevas = "Ananāss";
+            }
+
+            // Ja lietotājs visu ir uncheckojis tad Picas piedevums būs...
+            if (!senes.isSelected() && !pepperoni.isSelected() && !ananas.isSelected()) {
+            	PicasPiedevas = "Pepperoni";
+            }
+            
+            
+            // Debug prints
+            System.out.println("Picas izmērs: " + PicasIzmers);
+            System.out.println("Picas piedeva: " + PicasPiedevas);
+        };
+
+        S.addActionListener(klausitajs);
+        M.addActionListener(klausitajs);
+        XL.addActionListener(klausitajs);
+        
+        senes.addActionListener(klausitajs);
+        pepperoni.addActionListener(klausitajs);
+        ananas.addActionListener(klausitajs);
+        
 	}
+
+	
 
 }
